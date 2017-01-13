7e88d3dc11b3a64ef47033b5fa589c5eed64003f
kiwistaki <paquet.alex@gmail.com>
2017-01-13 09:51:54 -0500

    correction des for, ajout dune verification pour le nombre dargument

--- seq/lab1.c
+++ seq/lab1.c
@@ -3,59 +3,65 @@
 #include "sys/time.h"
 
 int main(int argc, const char* argv[]) {
-	//Code tire de l'exemple minuteur.c fourni sur le site du cours
-	double timeStart, timeEnd, Texec;
-	struct timeval tp;
-	gettimeofday (&tp, NULL); // Debut du chronometre
-	timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
 	
-	int prob = argv[0];
-	int valeur = argv[1];
-	int alteration = argv[2];
-	
-	int[8][8] matrix;
-	for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
-			usleep(1000);
-			matrix[i][j] = valeur;
-		}
-	}
-	
-	if(prob == 1){
-		for(int k = 1; k <= alteration; k++){
-			for(int i = 0; i < 8; i++){
-				for(int j = 0; j < 8; j++){
-					usleep(1000);
-					matrix[i][j] = matrix[i][j] + (i+j) * k;
-				}
+	if(argc ==3){
+		//Code tire de l'exemple minuteur.c fourni sur le site du cours
+		double timeStart, timeEnd, Texec;
+		struct timeval tp;
+		gettimeofday (&tp, NULL); // Debut du chronometre
+		timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+		
+		int prob = argv[0];
+		int valeur = argv[1];
+		int alteration = argv[2];
+		
+		int matrix[8][8];
+		for( i = 0; i < 8; i++){
+			for( j = 0; j < 8; j++){
+				usleep(1000);
+				matrix[i][j] = valeur;
 			}
 		}
-	}
-	else if(prob==2){
-		for(int k = 1; k <= alteration; k++){
-			for(int i = 0; i < 8; i++){
-				for(int j = 0; j < 8; j++){
-					if(j==0){
+		
+		if(prob == 1){
+			for( k = 1; k <= alteration; k++){
+				for( i = 0; i < 8; i++){
+					for( j = 0; j < 8; j++){
 						usleep(1000);
-						matrix[i][j] = matrix[i][j] + (i*k);
-					}else{
-						usleep(1000);
-						matrix[i][j] = matrix[i][j] + matrix[i][j-1] * k;
+						matrix[i][j] = matrix[i][j] + (i+j) * k;
 					}
 				}
 			}
 		}
-	}
-	
-	//Code tire de l'exemple minuteur.c fourni sur le site du cours
-	gettimeofday (&tp, NULL); // Fin du chronometre
-	timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
-	Texec = timeEnd - timeStart; //Temps d'execution en secondes
-	
-	for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
-			fprintf('%d',matrix[i][j]);
+		else if(prob==2){
+			for( k = 1; k <= alteration; k++){
+				for( i = 0; i < 8; i++){
+					for( j = 0; j < 8; j++){
+						if(j==0){
+							usleep(1000);
+							matrix[i][j] = matrix[i][j] + (i*k);
+						}else{
+							usleep(1000);
+							matrix[i][j] = matrix[i][j] + matrix[i][j-1] * k;
+						}
+					}
+				}
+			}
 		}
+		
+		//Code tire de l'exemple minuteur.c fourni sur le site du cours
+		gettimeofday (&tp, NULL); // Fin du chronometre
+		timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
+		Texec = timeEnd - timeStart; //Temps d'execution en secondes
+		
+		for( i = 0; i < 8; i++){
+			for( j = 0; j < 8; j++){
+				fprintf('%d',matrix[i][j]);
+			}
+		}
+		fprintf('%lf',Texec);
 	}
-	fprintf('%f',Texec);
+	else{
+		return -1;
+	}
 }
