8b965840ff44b29b9c3580d60e1eb4ce81b3e327
Alex <alexandre.paquet@polymtl.ca>
2017-01-22 19:42:47 -0500

    fini main, reste a faire chq fonction

--- par/lab1.c
+++ par/lab1.c
@@ -3,29 +3,61 @@
 #include "sys/time.h"
 #include "mpi.h"
 
-int main(int argc, const char* argv[]) {
-	//printf("%d", argc);
-	//fflush(stdout);
+// Probleme 1 pour le process maitre 
+void prob1Master(int matrix[8][8], int alteration, int nb_proc){
 	
-	if(argc == 4){
+}
+
+// Probleme 1 pour les process esclaves
+void prob1Slave(){
+	
+}
+
+// Probleme 2 pour le process maitre 
+void prob2Master(int matrix[8][8], int alteration, int nb_proc){
+	
+}
+
+// Probleme 2 pour les process esclaves
+void prob2Slave(){
+	
+}
+
+int main(int argc, const char* argv[]) {
+	if (argc != 4){
+		printf("Il manque des arguments! \n");
+		return 0;
+	}
+	
+	int err;
+	int master = 0;
+	int nb_proc;
+	int mon_id;
+	
+	//acceder aux parametres 
+	int prob = atoi(argv[1]);
+	int valeur = atoi(argv[2]);
+	int alteration = atoi(argv[3]);
 		
+	// Creation d'un Comm_World
+	err = MPI_Init(&argc, &argv);
+	if(err != MPI_SUCCESS){
+		printf("Probleme lors de l'initialisation de MPI. \n");
+		return -1;
+	}
+	err = MPI_Comm_rank(MPI_COMM_WORLD, &mon_id);
+	err = MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
+	
+	if(mon_id == 0){
 		//Code tire de l'exemple minuteur.c fourni sur le site du cours
 		double timeStart, timeEnd, Texec;
 		struct timeval tp;
 		gettimeofday (&tp, NULL); // Debut du chronometre
 		timeStart = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
 		
-		// Creation dun Comm_World
-		int err;
-		err = MPI_Init(&argc, &argv);
-		
-		//acceder aux parametres 
-		int prob = atoi(argv[1]);
-		int valeur = atoi(argv[2]);
-		int alteration = atoi(argv[3]);
 		
 		printf("================================================\n");
-		printf("PAR: prob %d, init %d, iter %d\n\n",prob, valeur, alteration);
+		printf("PAR: prob %d, init %d, iter %d\n\n", prob, valeur, alteration);
 		
 		int i,j,k;
 
@@ -36,52 +68,43 @@ int main(int argc, const char* argv[]) {
 				usleep(1000);
 				matrix[i][j] = valeur;
 			}
-		}
+		}	
 		
-		if(prob == 1){			
-			for( k = 1; k <= alteration; k++){
-				for( i = 0; i < 8; i++){
-					for( j = 0; j < 8; j++){
-						usleep(1000);
-						matrix[i][j] = matrix[i][j] + (i+j) * k;
-					}
-				}
-			}
+		switch(prob){
+			case 1:
+					prob1Master(matrix, alteration, nb_proc);
+					break;
+			case 2:
+					prob2Master(matrix, alteration, nb_proc);
+					break;
+			default:
+					printf("Choix de probleme inexistant!\n");
+					break;
 		}
-		else if(prob==2){
-			for( k = 1; k <= alteration; k++){
-				for( i = 0; i < 8; i++){
-					for( j = 0; j < 8; j++){
-						if(j==0){
-							usleep(1000);
-							matrix[i][j] = matrix[i][j] + (i*k);
-						}else{
-							usleep(1000);
-							matrix[i][j] = matrix[i][j] + matrix[i][j-1] * k;
-						}
-					}
-				}
-			}
-		}
-		
-		// Fermeture du Comm_World
-		err = MPI_Finalize();
 		
 		//Code tire de l'exemple minuteur.c fourni sur le site du cours
 		gettimeofday (&tp, NULL); // Fin du chronometre
 		timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
-		Texec = timeEnd - timeStart; //Temps d'execution en secondes
-		
-		printf("Matrice finale:\n");
-		for( i = 0; i < 8; i++){
-			printf("%d %d %d %d %d %d %d %d\n",matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7]);
+		Texec = timeEnd - timeStart; //Temps d'execution en secondes	
+	} else{
+		switch(prob){
+			case 1: prob1Slave();
+					break;
+			case 2: prob2Slave();
+					break;
 		}
-		printf("\n\n");
-		printf("Temps d'execution : %f\n",Texec);
-		printf("================================================\n");
-		return 0;
 	}
-	else{
-		return -1;
+	
+	// Fermeture du Comm_World
+	err = MPI_Finalize();
+	
+	printf("Matrice finale:\n");
+	for( i = 0; i < 8; i++){
+		printf("%d %d %d %d %d %d %d %d\n",matrix[i][0],matrix[i][1],matrix[i][2],matrix[i][3],matrix[i][4],matrix[i][5],matrix[i][6],matrix[i][7]);
 	}
+	printf("\n\n");
+	printf("Temps d'execution : %f\n",Texec);
+	printf("================================================\n");
+	return 0;
+
 }
