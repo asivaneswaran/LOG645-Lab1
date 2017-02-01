532ca3912e5f01a04cf2a7e61ec8affa83b98093
Akkun <paquet.alex@gmail.com>
2017-01-31 23:50:50 -0500

    refactor

--- par/lab1.c
+++ par/lab1.c
@@ -36,8 +36,27 @@ int main(int argc, const char* argv[]) {
 	err = MPI_Comm_rank(MPI_COMM_WORLD, &mon_id);
 	err = MPI_Comm_size(MPI_COMM_WORLD, &np);
 	
-	// Les 8 premiers process gerent les calculs
-	if(mon_id < 8){
+	// process maitre qui gerent la reponse
+	if(mon_id == master){
+		printf("================================================\n");
+		printf("PAR: prob %d, init %d, iter %d\n\n", prob, valeur, alteration);
+		
+		int j;
+		int resultat[8][8];
+		//Reception des messages dans lordre pour avoir le bon resultat
+		for(j=0;j<8;j++){
+			MPI_Recv(resultat[j],8,MPI_INT,j,0,MPI_COMM_WORLD,&statut);
+		}
+		
+		//Affichage de la matrice finale
+		printf("Matrice finale:\n");
+		int i;
+		for( i = 0; i < 8; i++){
+			printf("%d %d %d %d %d %d %d %d\n",resultat[i][0],resultat[i][1],resultat[i][2],resultat[i][3],resultat[i][4],resultat[i][5],resultat[i][6],resultat[i][7]);
+		}
+	}
+	// Les 8 autres process gerent les calculs
+	else if(mon_id != master){
 		
 		//Initialisation des lignes de la matrice (chaque ligne est identique au debut)
 		int i,k;
@@ -48,8 +67,8 @@ int main(int argc, const char* argv[]) {
 		
 		
 		//Dans le cas du probleme 1
-		if(prob==1){
-			//Meme code que sequentielle sauf change j par mon_id
+		if(prob == 1){
+			//Meme code que sequentielle sauf changement de j par mon_id
 			for(k=0;k<=alteration;k++){
 				for(i=0;i<8;i++){
 					usleep(1000);
@@ -57,8 +76,8 @@ int main(int argc, const char* argv[]) {
 				}
 			}
 		//Dans le cas du probleme 2
-		}else if(prob ==2){
-			//Meme code que sequentielle sauf change j par mon_id
+		}else if(prob == 2){
+			//Meme code que sequentielle sauf changement de j par mon_id
 			for(k=0;k<=alteration;k++){
 				for(i=0;i<8;i++){
 					if(i==0){
@@ -80,30 +99,12 @@ int main(int argc, const char* argv[]) {
 		//envoie les donnees au process maitre
 		MPI_Send(&matrix, 8, MPI_INT, 8, 0, MPI_COMM_WORLD);
 	}
-	// process maitre qui gerent la reponse
-	if(mon_id == master){
-		printf("================================================\n");
-		printf("PAR: prob %d, init %d, iter %d\n\n", prob, valeur, alteration);
-		
-		int j;
-		int resultat[8][8];
-		//Reception des messages dans lordre pour avoir le bon resultat
-		for(j=0;j<8;j++){
-			MPI_Recv(resultat[j],8,MPI_INT,j,0,MPI_COMM_WORLD,&statut);
-		}
-		
-		//Affichage de la matrice finale
-		printf("Matrice finale:\n");
-		int i;
-		for( i = 0; i < 8; i++){
-			printf("%d %d %d %d %d %d %d %d\n",resultat[i][0],resultat[i][1],resultat[i][2],resultat[i][3],resultat[i][4],resultat[i][5],resultat[i][6],resultat[i][7]);
-		}
-	}
+	
 	
 	//Fermeture du Comm_World
 	MPI_Finalize();
 	
-	if(mon_id== master){
+	if(mon_id == master){
 		//Code tire de l'exemple minuteur.c fourni sur le site du cours
 		gettimeofday (&tp, NULL); // Fin du chronometre
 		timeEnd = (double) (tp.tv_sec) + (double) (tp.tv_usec) / 1e6;
